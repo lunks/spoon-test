38905968ceb949320ed394e8e3206ec5ec4462c9
Pedro Nascimento <pnascimento@gmail.com>
2020-08-30 18:40:33 -0300

    App structure using Recipe model

--- app.rb
+++ app.rb
@@ -1,8 +1,16 @@
 require 'bundler'
 Bundler.require(:default)
 
+require './recipe'
+
 class App < Sinatra::Base
   get '/' do
-    'Hello world!'
+    @recipes = Recipe.all
+    erb :index, layout: :main
+  end
+
+  get '/:id' do
+    @recipe = Recipe.new(params[:id])
+    erb :show, layout: :main
   end
 end
